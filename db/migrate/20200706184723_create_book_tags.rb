79dcad81ae37877196beb87b67859e31e18d5a65
NicoleQiu99 <nicolepeng99@gmail.com>
2020-07-06 13:08:01 -0700

    first commit

---
+++ db/migrate/20200706184723_create_book_tags.rb
@@ -0,0 +1,12 @@
+class CreateBookTags < ActiveRecord::Migration[6.0]
+  def change
+    create_table :book_tags do |t|
+      t.integer :book_id
+      t.integer :tag_id
+      t.belongs_to :book, null: false, foreign_key: true
+      t.belongs_to :tag, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
