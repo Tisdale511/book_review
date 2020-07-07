ad0e5b1a0fe37e557be3222ffc80a66d579483ab
darrickpang <dkypang@yahoo.com>
2020-07-07 16:02:35 -0700

    added a few more stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   #resources :book_tags 
   resources :tags
-  resources :reviews
+  resources :reviews, only: [:new, :create, :edit, :update, :destroy]
   resources :books
-  resources :users
+  resources :users, only: [:new, :create, :edit, :update, :destroy]
 
   get "/signup", to: "users#new", as: :signup
   get "/login", to: "sessions#new"
