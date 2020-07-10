2ba355b3d1a3e527ee161f4c96d08a6f875082a8
NicoleQiu99 <nicolepeng99@gmail.com>
2020-07-10 11:01:56 -0700

    css with header and footer

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,13 +1,18 @@
 class TagsController < ApplicationController
+  def index
+  end
+
   def show
+    @book = Book.find(params[:book_id])
   end
   def new
     @tag = Tag.new
   end
 
   def create
-    @tag = Tag.create(tags_params)
-    redirect_to new_book_path
+    @book = Book.find(params[:book_id])
+    @tag = Tag.create(name: tags_params[:name])
+    redirect_to book_path(@book)
   end
 
   private
